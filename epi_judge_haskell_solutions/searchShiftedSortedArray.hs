7a926bad5b16dd052f021510190ceab9f10db457
Amogh Rathore <amoghdroid09@gmail.com>
2020-04-04 21:25:11 +0900

    small change in searchShiftedSortedArray

--- epi_judge_haskell_solutions/searchShiftedSortedArray.hs
+++ epi_judge_haskell_solutions/searchShiftedSortedArray.hs
@@ -3,13 +3,14 @@ import qualified Data.Vector as V
 
 search :: V.Vector Int -> Int 
 search xs = f 0 (V.length xs - 1) where 
-    f lo hi | lo == hi = lo 
-    f lo hi | hi == lo + 1 = if xs V.! lo < xs V.! hi then lo else hi 
-    f lo hi =
-        let m = (lo + hi) `quot` 2
-        in  if xs V.! m < V.last xs 
-                then f lo m 
-                else f m hi 
+    f lo hi 
+        | lo == hi = lo 
+        | hi == lo + 1 = if xs V.! lo < xs V.! hi then lo else hi 
+        | otherwise =
+            let m = (lo + hi) `quot` 2
+            in  if xs V.! m < V.last xs 
+                    then f lo m 
+                    else f m hi 
 
 main = goTest
     search 
